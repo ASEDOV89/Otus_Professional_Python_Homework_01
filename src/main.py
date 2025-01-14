573bba88263bd4fc75508e5d015699f7fb42b892
ASEDOV89 <a.sedov@x5.ru>
2025-01-14 20:16:07 +0300

    Приведение кода в соответствие с pep8

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
-from app.module import err, func
+import sys
+from pathlib import Path
+from src.app.module import err, func
+
+
+sys.path.append(str(Path(__file__).resolve().parent))
 
 if __name__ == "__main__":
     print(func())
